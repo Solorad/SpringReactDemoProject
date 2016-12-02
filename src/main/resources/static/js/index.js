203491d685c205b1e1cda5f577a48b9d8817a8c4
Evgenii Morenkov <yarad8613@yandex.ru>
2016-12-02 08:56:32 +0300

    Start work with csrf tokens

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
+const csrfTokenHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+const csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+
 // We require the routes and render to the DOM using ReactDOM API
 ReactDOM.render(
     <Router history={browserHistory} routes={routes} />,
