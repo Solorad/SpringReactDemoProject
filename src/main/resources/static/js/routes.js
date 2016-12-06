431b8ccd2354e582c2a2b0595a2721404d304a9b
emorenkov <emorenkov@luxoft.com>
2016-12-06 20:12:53 +0300

    another update

--- src/main/resources/static/js/routes.js
+++ src/main/resources/static/js/routes.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import BookPage from './books/BookPage';
 
+function App({ url, isLoggedIn }) {
+    return (
+            !isLoggedIn ? <PageLogin /> :
+            url === '/' ? <PageMain /> :
+            url === '/book' ? <PageAbout /> :
+            <Page404 />
+);
+}
+
 export default (
-    <Route path="/" component={BookPage}>
-    </Route>
+    <Route path="*" component={AppRouter} />
 );
