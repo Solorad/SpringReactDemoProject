d7123919b78e52784b353eef81620d69630f7646
emorenkov <emorenkov@luxoft.com>
2016-12-23 18:07:17 +0300

    Update frontend

--- src/main/resources/static/js/pages/BookPage.js
+++ src/main/resources/static/js/pages/BookPage.js
@@ -1,82 +1,97 @@
-import React from "react";
+import React, {Component} from "react";
 import "babel-polyfill";
+import {Link} from 'react-router';
 import stompClient from "../common/websocket-listener"; // function to hop multiple links by "rel"
 import request from '../common/ajax';
-import {Link, browserHistory } from 'react-router';
 
 import BooksTable from "../books/BooksTable";
 
 
-function BookPage({page, limit}) {
-  request("/api/books?page=" + page + "&limit=" + limit)
-    .then((data) => {
-      const totalPages = data ? data.page.totalPages : 0;
-      const lastPage = Math.max(0, totalPages - 1);
-      const books = data ? data._embedded.books : [];
-      const bookParam = location.query && location.query.book;
+class BookPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books : [],
+      page : 0,
+      totalPages : 1,
+      lastPage : 1
+    };
+  }
 
-      console.log(bookParam);
+  componentDidMount() {
+    request("/api/books?page=" + this.props.page + "&limit=" + this.props.limit)
+      .then((data) => {
+        this.setState({
+          totalPages: data.page.totalPages,
+          lastPage: totalPages - 1,
+          books: data._embedded.books,
+          bookParam: location.query && location.query.book,
+        });
+      });
 
-      // stompClient.register([
-      //   {route: '/topic/newBook', callback: browserHistory.push("/books")},
-      //   {route: '/topic/updateBook', callback: browserHistory.push("/books")},
-      //   {route: '/topic/deleteBook', callback: browserHistory.push("/books")}
-      // ]);
 
-      function updateSelect(event){
-        console.log(event);
-        window.location.href=event.value;
-      }
+    if (this.props.page < 0 || this.state.page > this.state.lastPage || this.props.limit < 1) {
+      return <div>404</div>;
+    }
 
-      if (page < 0 || page > lastPage || limit < 1) {
-        return <div>404</div>;
-      }
+    // stompClient.register([
+    //   {route: '/topic/newBook', callback: this.refreshAndGoToLastPage},
+    //   {route: '/topic/updateBook', callback: this.refreshCurrentPage},
+    //   {route: '/topic/deleteBook', callback: this.refreshCurrentPage}
+    // ]);
+  }
 
-      return (
-        <div className="books">
-          <div className="books__header">
-            <div className="books__title">Books — Page {page + 1} of {totalPages}</div>
-            <div className="books__controls">
-              <Link
-                className="books__create"
-                to={location.pathname + location.search + (location.search ? '&book=new' : '?book=new')}
-              >
-                Create
-              </Link>
-              <select onChange={updateSelect}>
-                <option value="/books?limit=2">2</option>
-                <option value="/books?limit=5">5</option>
-                <option value="/books?limit=7">7</option>
-                <option value="/books?limit=10">10</option>
-              </select>
-            </div>
+  updateSelect(event) {
+    console.log(event);
+    window.location.href = event.value;
+  }
+
+  render() {
+
+
+    return (
+      <div className="books">
+        <div className="books__header">
+          <div className="books__title">Books — Page {this.props.page + 1} of {this.state.totalPages}</div>
+          <div className="books__controls">
+            <Link
+              className="books__create"
+              to={location.pathname + location.search + (location.search ? '&book=new' : '?book=new')}
+            >
+              Create
+            </Link>
+            <select onChange={this.updateSelect}>
+              <option value="/books?limit=2">2</option>
+              <option value="/books?limit=5">5</option>
+              <option value="/books?limit=7">7</option>
+              <option value="/books?limit=10">10</option>
+            </select>
           </div>
-          <div className="books__table">
-            <BooksTable books={books} pageSize={limit} page={page}/>
-          </div>
-          <div className="books__paginator">
-            <PaginatorLink page="0" limit={limit} disabled={page === 0}>&lt;&lt;</PaginatorLink>
-            <PaginatorLink page={page - 1} limit={limit} disabled={page === 0}>&lt;</PaginatorLink>
-            <PaginatorLink page={page + 1} limit={limit} disabled={page === lastPage}>&gt;</PaginatorLink>
-            <PaginatorLink page={lastPage} limit={limit} disabled={page === lastPage}>&gt;&gt;</PaginatorLink>
-          </div>
-
-          {bookParam && (
-            <Modal closeUrl={`/books?page=${page}&limit=${limit}`}>
-              <BookEditor book={bookParam === 'new' ? null : null  }/>
-            </Modal>
-          )}
         </div>
-      );
-    })
-    .catch((err) => {
-      return (
-        <div>Error occurred on load data! {err}</div>
-      );
-    });
+        <div className="books__table">
+          <BooksTable books={this.state.books} pageSize={this.props.limit} page={this.props.page}/>
+        </div>
+        <div className="books__paginator">
+          <PaginatorLink page="0" limit={this.props.limit} disabled={this.props.page === 0}>&lt;&lt;</PaginatorLink>
+          <PaginatorLink page={this.props.page - 1} limit={this.props.limit}
+                         disabled={this.props.page === 0}>&lt;</PaginatorLink>
+          <PaginatorLink page={this.props.page + 1} limit={this.props.limit}
+                         disabled={this.props.page === this.state.lastPage}>&gt;</PaginatorLink>
+          <PaginatorLink page={this.state.lastPage} limit={this.props.limit}
+                         disabled={this.state.page === this.state.lastPage}>&gt;&gt;</PaginatorLink>
+        </div>
+
+        {this.state.bookParam && (
+          <Modal closeUrl={`/books?page=${this.props.page}&limit=${this.props.limit}`}>
+            <BookEditor book={this.state.bookParam === 'new' ? null : null  }/>
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
 
-function PaginatorLink({ page, limit, disabled, children }) {
+function PaginatorLink({page, limit, disabled, children}) {
   return (
     <Link
       className="books__paginatorLink"
