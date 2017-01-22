e3bcf0295b8a6a071159784754b1344ad317cb2b
Evgenii Morenkov <yarad8613@yandex.ru>
2017-01-23 00:55:24 +0300

    Update

---
+++ src/main/java/com/morenkov/repository/MongodbAccountRepository.java
@@ -0,0 +1,10 @@
+package com.morenkov.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by solorad on 23.01.17.
+ */
+@Repository
+public class MongodbAccountRepository {
+}
